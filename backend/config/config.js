87f9a5c79defe6291ab8a3269f7be12e627b5b2d
Gajanan Chaudhari <gajananchaudhari97724@gmail.com>
2025-07-22 14:09:16 +0530

    .

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,25 +1,25 @@
-require('dotenv').config(); // Make sure this line is at the top
+require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.POSTGRES_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-  },
- production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // for Render or Heroku
-      },
-    },
-},
+    dialect: 'postgres', // 👈 IMPORTANT
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres', // 👈
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres', // 👈
+  },
 };
