3df6c3df799b7960646f76b10cee054838b316a4
Gajanan Chaudhari <gajananchaudhari97724@gmail.com>
2025-09-09 18:11:58 +0530

    .

--- frontend/scripts/generate-sitemap.js
+++ frontend/scripts/generate-sitemap.js
@@ -1,6 +1,11 @@
-const { createSitemap } = require('sitemap');
-const fs = require('fs');
-const path = require('path');
+import { createSitemap } from 'sitemap';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// ES modules don't have __dirname, so we create it
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const hostname = 'https://www.getyourprojectdone.in';
 
